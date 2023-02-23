31c16eb65bfa9d3a3eda749a7df1639b95396d79
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-23 15:42:31 +0100

    add pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>comics</title>
     @vite('resources/js/app.js')
 </head>
 
@@ -18,9 +18,9 @@
             <div class="nav_right">
 
                 <ul>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="{{ route('characters') }}">CHARACTERS</a></li>
+                    <li><a href="{{ url('/') }}">COMICS</a></li>
+                    <li><a href="{{ route('movies') }}">MOVIES</a></li>
                     <li><a href="#">TV</a></li>
                     <li><a href="#">GAMES</a></li>
                     <li><a href="#">COLLECTIBLES</a></li>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
+
+Route::get('/movies', function () {
+    return view('movies');
+})->name('movies');
