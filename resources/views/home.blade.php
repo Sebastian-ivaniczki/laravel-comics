c3f6a612b9ebd9120f5108f08fdf75749fb56f1d
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-23 15:15:13 +0100

    add img and home htm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics</title>
+    <title>Document</title>
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-
+    <header>
+        <div>
+            <figure>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo.png">
+            </figure>
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </div>
+    </header>
 </body>
 
 </html>
